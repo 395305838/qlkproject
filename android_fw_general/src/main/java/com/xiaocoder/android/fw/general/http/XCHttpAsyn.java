1ef220a6219e989b0a82e38f6f9036e580ad9c1d
xiaocoder <xiaocoder@163.com>
2015-08-17 15:16:06 +0800

    一点注释

--- android_fw_general/src/main/java/com/xiaocoder/android/fw/general/http/XCHttpAsyn.java
+++ android_fw_general/src/main/java/com/xiaocoder/android/fw/general/http/XCHttpAsyn.java
@@ -11,15 +11,22 @@ import com.xiaocoder.android.fw.general.application.XCConfig;
 /*单例
  * 1如果是上传文件:
  * 	可以 params.put("字段", new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/0912/compass.png"));
- * 
+ *  同时支持流和字节
+ *
  * 2不管是失败还是完成都会调用onFinish()方法
  * 
- * 3下载数据可以用byte[]
+ * 3下载数据可以用byte[]，而不用result_bean
  * 
  * 4是异步的
- *  * isShowDialog 是否在加载网络时显示dialog
+ * isShowDialog 是否在加载网络时显示dialog
  * isNeting 是只有当前网络请求完了, 再点击访问网络时才有效, 如果正在请求网络或者说网络访问没有结束,那么此时点击访问网络无效 , 默认是同一时刻只能有一个网络在请求
- * isAllowCon
+ * isAllowConcurrent 如果为true，则允许同时访问多个网络连接
+ *
+ * 5 onRetry（） onStart（） onCancle（） onProgress（）
+ *
+ * 6  PersistentCookieStore 见github文档
+ *
+ * 7  Adding HTTP Basic Auth credentials 见github文档
  */
 
 public class XCHttpAsyn {
