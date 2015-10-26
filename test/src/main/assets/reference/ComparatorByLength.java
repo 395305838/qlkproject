4b82470029bef4477d2a3da065a7e5d4d37ec79b
xiaocoder <xiaocoder@163.com>
2015-10-26 18:32:00 +0800

    。。

---
+++ test/src/main/assets/reference/ComparatorByLength.java
@@ -0,0 +1,18 @@
+package reference;
+
+import java.util.Comparator;
+
+public class _13ComparatorByLength implements Comparator {
+
+	@Override
+	public int compare(Object o1, Object o2) {
+		
+		String s1 = (String)o1;
+		String s2 = (String)o2;
+		
+		int temp = s1.length() - s2.length();
+		
+		return temp==0?s1.compareTo(s2):temp;
+	}
+
+}
